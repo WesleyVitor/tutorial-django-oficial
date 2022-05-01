e4b7db3b01c9623c9bb2429f1a544242170eb09d
WesleyVitor <wesleydemorais@outlook.com.br>
2022-05-01 14:51:40 -0300

    feat: Parte 2 Finalizada

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,10 +5,17 @@ class Question(models.Model):
     question_text = models.TextField(max_length=200)
     pub_date = models.DateTimeField(verbose_name="date published")
 
+    def __str__(self) -> str:
+        return self.question_text
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice
+
 
 
 # Create your models here.
