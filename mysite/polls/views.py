5bc6b7ab299118c9779def2411cba59d434492db
WesleyVitor <wesleydemorais@outlook.com.br>
2022-05-04 09:50:13 -0300

    feat: Escrevendo views genéricas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,29 +3,20 @@ from django.template import loader
 from polls.models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]    
 
-def index(request:HttpRequest):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context= {
-        "latest_question_list":latest_question_list
-    }
-    return HttpResponse(template.render(context, request))
-    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/details.html"
 
-def details(request, question_id:str):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exists!")
-    # question = get_object_or_404(Question, pk=question_id) pode ser utilizado com shortcuts para desacoplar 
-    # Template com View
-    template = loader.get_template("polls/details.html")
-    return HttpResponse(template.render({"question":question}, request))
-
-def results(request, question_id:str):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html',{'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id:str):
     question = get_object_or_404(Question, pk=question_id)
