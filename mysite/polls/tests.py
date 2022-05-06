49e86f3e948c0727e8535e0f7a0cee771fdf17d1
WesleyVitor <wesleydemorais@outlook.com.br>
2022-05-06 13:43:23 -0300

    feat: Adicionando arquivos estáticos

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,5 +1,7 @@
 
+from ast import arg
 from http import client
+from unittest import TestResult
 from django.test import TestCase
 from django.utils import timezone
 from datetime import timedelta
@@ -53,4 +55,20 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse("polls:index"))
         self.assertContains(response, "No polls are available")
         self.assertQuerysetEqual(response.context["latest_question_list"], [])
+    
+class QuestionDetailsViewTests(TestCase):
+    client = Client()
+    def test_future_question(self):
+
+        future_question = create_question(question_text="Future Question", days=5)
+        url = reverse("polls:details", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_past_question(self):
+
+        past_question = create_question(question_text="Past Question", days=-5)
+        url = reverse("polls:details", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
         
